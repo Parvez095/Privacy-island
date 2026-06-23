2f67941ae5fc278c0e7115107e753a42467162d2
Mohammed Parvez <74482985+Parvez095@users.noreply.github.com>
2026-06-24 02:45:19 +0530

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,6 +19,14 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            isDebuggable = false
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
 }
 
 kotlin {
